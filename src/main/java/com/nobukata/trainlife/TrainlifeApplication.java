70e3e67316d20ff65af03b1be5b6fdc87c9eb1ec
Andrey Bodrov <code.drummer87@gmail.com>
2025-09-23 18:56:12 +0600

    init repository

---
+++ src/main/java/com/nobukata/trainlife/TrainlifeApplication.java
@@ -0,0 +1,13 @@
+package com.nobukata.trainlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobukata/trainlife/TrainlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobukata.trainlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
