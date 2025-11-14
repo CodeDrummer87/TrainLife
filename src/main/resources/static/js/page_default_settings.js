31a6dcd495f1b884422000f5c96ceb00cc0755b9
Andrey Bodrov <code.drummer87@gmail.com>
2025-11-15 04:17:37 +0600

    traffic light selection was added

--- src/main/resources/static/js/page_default_settings.js
+++ src/main/resources/static/js/page_default_settings.js
@@ -1,7 +1,8 @@
 const stationUrl = 'http://localhost:8080/api/v1/stations/allocations';
 let allStations = [];
 const trafficLightUrl = 'http://localhost:8080/api/v1/traffic-lights';
-let trafficLightList = [];
+let departureTrafficLightList = [];
+let arrivalTrafficLightList = [];
 
 async function initStations() {
     try {
@@ -30,6 +31,32 @@ async function fetchStations() {
         return [];
     }
 }
+
+async function fetchTrafficLights(span_station, defaultRecord) {
+    console.log(`Для ${defaultRecord}: span_station = ${span_station.innerText} | s_train_number = ${s_train_number.innerText}`);
+    if (span_station.innerText !== defaultRecord && s_train_number.innerText !== 'номер') {
+        const stationId = span_station.dataset.id;
+        const isEven = parseInt(s_train_number.innerText) % 2 === 0;
+        try {
+            const uri = `/station/${stationId}?isEven=${isEven}`;
+            const response = await fetch(trafficLightUrl + uri);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            const key = defaultRecord === 'станция отправления' ?
+                'departure_traffic_light_list' : 'arrival_traffic_light_list';
+            sessionStorage.setItem(key, JSON.stringify(result));
+
+            return result;
+        } catch(error) {
+            console.error("Ошибка выполнения запроса на извлечение светофоров: " + error);
+            return [];
+        }
+    }
+}
 //.::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 setValueAndColor(s_attendance, 'attendance_date', 'установить');
 setValueAndColor(s_series, 'series', 'серия');
@@ -45,6 +72,9 @@ setValueAndColor(s_train_weight, 'train_weight', 'масса поезда');
 setValueAndColor(s_number_of_axis, 'number_of_axis', 'количество осей');
 setValueAndColor(s_conditional_length, 'conditional_length', 'условная длина');
 setValueAndColor(s_tailcar_number, 'tailcar_number', 'номер хвостового вагона');
+setValueAndColor(s_departure_traffic_light, 'departure_traffic_light', '(светофор)');
+setValueAndColor(s_arrival_traffic_light, 'arrival_traffic_light', '(светофор)');
+checkTrafficLightList();
 
 function setValueAndColor(element, item, value) {
     element.innerText = sessionStorage.getItem(item) === null ?
@@ -57,4 +87,9 @@ function checkValueStation(stationElement, trainElement, trafficLightElement, de
     if ((defaultStation !== stationElement.innerText) && (defaultTrainNumber !== trainElement.innerText)) {
         trafficLightElement.classList.toggle('inactive');
     }
+}
+
+function checkTrafficLightList() {
+    departureTrafficLightList = JSON.parse(sessionStorage.getItem('departure_traffic_light_list'));
+    arrivalTrafficLightList = JSON.parse(sessionStorage.getItem('arrival_traffic_light_list'));
 }
